cf6877eef9b78501d65e8c86d0a2c543464178b4
Nadeem S <venomreborn@gmail.com>
2026-03-26 11:47:38 +0000

    Update petti v0.6.4 formula

--- Formula/petti.rb
+++ Formula/petti.rb
@@ -1,26 +1,26 @@
 class Petti < Formula
   desc "Terminal UI for browsing installed packages across package managers"
   homepage "https://github.com/707/petti"
-  version "0.6.3"
+  version "0.6.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_darwin_arm64.tar.gz"
-      sha256 "ad32eee23338602d625fdf32e8f433463da09864f9cb1edd6264d9a89cf7930a"
+      sha256 "e5c56c4db4381a42629761f7ac650ef681a5cb851158ce1c76de8a8c82394179"
     else
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_darwin_amd64.tar.gz"
-      sha256 "9f2245704486120a67c605972bb204e660692bb2efd56eeff0d8a0dee6f2021c"
+      sha256 "647939e01f0c4c3ce06f7e16408365541ab0bc03a971139ffce2788e01409605"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_linux_arm64.tar.gz"
-      sha256 "963d1636942050c2c039b67c3fa06c87b5bf3af2c614a0c9abc97171c938ff32"
+      sha256 "4f500a7af354d0949d4f6e9fc49be0952d1dce056ad73dbf2d8749d0c9dcfbca"
     else
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_linux_amd64.tar.gz"
-      sha256 "4cc999be9f4d6093233885d5357d6bf92cc24330a46f22c1560a6f32ffcffbfb"
+      sha256 "e28a6feb948c5bd390f5c1ed331f1439370dbb5e0042fcf89173f4216073bef1"
     end
   end
 
