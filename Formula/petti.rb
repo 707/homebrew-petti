3118a1656a3bca1bb2d6460011408ec5dc7c87bc
Nadeem S <venomreborn@gmail.com>
2026-03-26 10:36:48 +0000

    Update petti v0.6.3 checksums

--- Formula/petti.rb
+++ Formula/petti.rb
@@ -7,20 +7,20 @@ class Petti < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_darwin_arm64.tar.gz"
-      sha256 "ea40875b4bf2ce764bbc9fc9f7b1a5cf8f67aeb3daa34c7520a06467e82f450f"
+      sha256 "ad32eee23338602d625fdf32e8f433463da09864f9cb1edd6264d9a89cf7930a"
     else
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_darwin_amd64.tar.gz"
-      sha256 "bed0d51dc4e286ea7e61c5a52fd58f1dfa25ea61ebc13aec0631a77aa0594b43"
+      sha256 "9f2245704486120a67c605972bb204e660692bb2efd56eeff0d8a0dee6f2021c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_linux_arm64.tar.gz"
-      sha256 "019eaa00fb27f7f35d9dc0b71b27dd37a33e9163f97b276b1485dca4471f38ca"
+      sha256 "963d1636942050c2c039b67c3fa06c87b5bf3af2c614a0c9abc97171c938ff32"
     else
       url "https://github.com/707/petti/releases/download/v#{version}/petti_#{version}_linux_amd64.tar.gz"
-      sha256 "d2c55fd61ce8345f2f2ac3adf1750425c1aa6d1d43b1d58fea8fb74db3fe0925"
+      sha256 "4cc999be9f4d6093233885d5357d6bf92cc24330a46f22c1560a6f32ffcffbfb"
     end
   end
 
